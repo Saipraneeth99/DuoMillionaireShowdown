07b8714d336e4c3154a2d01f17a72fcdf7e3e0a3
Saipraneeth99 <saipraneeth.dulam@gmail.com>
2023-10-22 19:30:01 -0400

    Buzzer Component Created but need fixes

--- src/components/Buzzer.jsx
+++ src/components/Buzzer.jsx
@@ -1,1 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import useSound from "use-sound";
+import tiktok from "../audio/ticktock.mp3";
+import wrongSound from "../audio/wrong answer.mp3";
+import correctSound from "../audio/correct.mp3";
+import '../styles/buzzer.css';
+
+const Buzzer = ({ question, onAnswerSelected, setTimerRunning }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(tiktok);
+  const [correctAnswerSound] = useSound(correctSound);
+  const [wrongAnswerSound] = useSound(wrongSound);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setTimerRunning(false);
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+    delay(5000, () => {
+      if (a.correct) {
+        correctAnswerSound();
+        delay(1000, () => {
+          setSelectedAnswer(null);
+          setTimerRunning(true);
+          onAnswerSelected(true);
+        });
+      } else {
+        wrongAnswerSound();
+        delay(1000, () => {
+          onAnswerSelected(false);
+        });
+      };
+    });
+  };
+
+  return (
+    <div className='buzzer'>
+      <div className="question">
+        {question?.question}
+      </div>
+      <div className="options">
+        {question?.answers.map((answer, index) => (
+          <div 
+            className={selectedAnswer === answer ? className : "answer"}
+            key={index} 
+            onClick={() => handleClick(answer)}
+          >
+            {answer.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+};
+
+export default Buzzer;
