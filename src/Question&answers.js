f7f86123f18353e142c131971b0054036a09d648
Sai Praneeth Dulam <61410168+Saipraneeth99@users.noreply.github.com>
2023-11-27 01:36:34 -0500

    Added new questions Question&answers.js

--- src/Question&answers.js
+++ src/Question&answers.js
@@ -1,100 +1,419 @@
 var Questions = [
+    {
+        "question": "What is the chemical formula for table salt?",
+        "options": ["NaCl", "H2O", "CO2", "C12H22O11"],
+        "answer": "NaCl"
+    },
+    {
+        "question": "Who painted 'Starry Night'?",
+        "options": ["Vincent Van Gogh", "Pablo Picasso", "Claude Monet", "Leonardo da Vinci"],
+        "answer": "Vincent Van Gogh"
+    },
+    {
+        "question": "What is the longest river in the USA?",
+        "options": ["Mississippi River", "Colorado River", "Missouri River", "Rio Grande"],
+        "answer": "Missouri River"
+    },
+    {
+        "question": "What is the capital of Australia?",
+        "options": ["Sydney", "Melbourne", "Perth", "Canberra"],
+        "answer": "Canberra"
+    },
+    {
+        "question": "Who wrote the 'Harry Potter' series?",
+        "options": ["J.K. Rowling", "Stephen King", "Roald Dahl", "J.R.R. Tolkien"],
+        "answer": "J.K. Rowling"
+    },
+    {
+        "question": "What is the largest organ in the human body?",
+        "options": ["Heart", "Liver", "Skin", "Brain"],
+        "answer": "Skin"
+    },
+    {
+        "question": "Which planet is closest to the sun?",
+        "options": ["Earth", "Venus", "Mercury", "Mars"],
+        "answer": "Mercury"
+    },
+    {
+        "question": "In which country is the Taj Mahal located?",
+        "options": ["India", "Pakistan", "Bangladesh", "Afghanistan"],
+        "answer": "India"
+    },
+    {
+        "question": "Who composed the music for the ballet 'The Nutcracker'?",
+        "options": ["Wolfgang Amadeus Mozart", "Ludwig van Beethoven", "Johann Sebastian Bach", "Pyotr Ilyich Tchaikovsky"],
+        "answer": "Pyotr Ilyich Tchaikovsky"
+    },
+    {
+        "question": "What is the capital of Canada?",
+        "options": ["Toronto", "Vancouver", "Montreal", "Ottawa"],
+        "answer": "Ottawa"
+    },
+    {
+        "question": "Who is known as the 'Father of Computer Science'?",
+        "options": ["Charles Babbage", "Alan Turing", "John von Neumann", "Bill Gates"],
+        "answer": "Alan Turing"
+    },
+    {
+        "question": "What does DNA stand for?",
+        "options": ["Deoxyribonucleic Acid", "Dinucleic Acid", "Deoxynucleic Acid", "Dioxyribonucleic Acid"],
+        "answer": "Deoxyribonucleic Acid"
+    },
+    {
+        "question": "In which sport would you perform a 'slam dunk'?",
+        "options": ["Football", "Baseball", "Basketball", "Volleyball"],
+        "answer": "Basketball"
+    },
+    {
+        "question": "What is the main language spoken in Brazil?",
+        "options": ["Spanish", "Portuguese", "English", "French"],
+        "answer": "Portuguese"
+    },
+    {
+        "question": "What is the capital of Spain?",
+        "options": ["Barcelona", "Valencia", "Seville", "Madrid"],
+        "answer": "Madrid"
+    },
+    {
+        "question": "What is the largest continent on Earth?",
+        "options": ["Asia", "Africa", "Europe", "North America"],
+        "answer": "Asia"
+    },
+    {
+        "question": "Which country gave the Statue of Liberty to the USA?",
+        "options": ["France", "United Kingdom", "Germany", "Italy"],
+        "answer": "France"
+    },
+    {
+        "question": "What type of animal is a 'flemish giant'?",
+        "options": ["Dog", "Cat", "Rabbit", "Horse"],
+        "answer": "Rabbit"
+    },
+    {
+        "question": "Who is known as the 'Queen of Pop'?",
+        "options": ["Madonna", "Beyonce", "Lady Gaga", "Whitney Houston"],
+        "answer": "Madonna"
+    },
+    {
+        "question": "What is the capital city of Germany?",
+        "options": ["Munich", "Frankfurt", "Berlin", "Hamburg"],
+        "answer": "Berlin"
+    },
+    {
+        "question": "Which country is famous for its tulips?",
+        "options": ["Netherlands", "France", "Belgium", "Germany"],
+        "answer": "Netherlands"
+    },
+    {
+        "question": "Who wrote 'The Great Gatsby'?",
+        "options": ["F. Scott Fitzgerald", "Ernest Hemingway", "John Steinbeck", "Mark Twain"],
+        "answer": "F. Scott Fitzgerald"
+    },
+    {
+        "question": "What is the smallest planet in our Solar System?",
+        "options": ["Mercury", "Venus", "Mars", "Pluto"],
+        "answer": "Mercury"
+    },
+    {
+        "question": "What is the capital of Italy?",
+        "options": ["Milan", "Rome", "Naples", "Venice"],
+        "answer": "Rome"
+    },
+    {
+        "question": "Which bird is often associated with delivering babies?",
+        "options": ["Stork", "Swan", "Eagle", "Pelican"],
+        "answer": "Stork"
+    },
+    {
+        "question": "What is the primary color of the Chinese flag?",
+        "options": ["Green", "Blue", "Yellow", "Red"],
+        "answer": "Red"
+    },
+    {
+        "question": "In which city is the Louvre Museum located?",
+        "options": ["Paris", "London", "New York", "Berlin"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Who wrote 'To Kill a Mockingbird'?",
+        "options": ["Harper Lee", "George Orwell", "J.D. Salinger", "Virginia Woolf"],
+        "answer": "Harper Lee"
+    },
+    {
+        "question": "What is the main component of the sun?",
+        "options": ["Helium", "Hydrogen", "Oxygen", "Nitrogen"],
+        "answer": "Hydrogen"
+    },
+    {
+        "question": "Which ocean is the smallest?",
+        "options": ["Pacific", "Atlantic", "Indian", "Arctic"],
+        "answer": "Arctic"
+    },
+    {
+        "question": "What is the capital city of South Korea?",
+        "options": ["Seoul", "Busan", "Incheon", "Daegu"],
+        "answer": "Seoul"
+    },
+    {
+        "question": "Which famous scientist developed the theory of general relativity?",
+        "options": ["Albert Einstein", "Isaac Newton", "Stephen Hawking", "Nikola Tesla"],
+        "answer": "Albert Einstein"
+    },
+    {
+        "question": "What is the national animal of Canada?",
+        "options": ["Beaver", "Moose", "Bald Eagle", "Grizzly Bear"],
+        "answer": "Beaver"
+    },
+    {
+        "question": "What is the pH of pure water?",
+        "options": ["7", "5", "9", "11"],
+        "answer": "7"
+    },
+    {
+        "question": "Which country is famous for its fjords?",
+        "options": ["Norway", "Sweden", "Finland", "Iceland"],
+        "answer": "Norway"
+    },
+    {
+        "question": "Who was the first President of the United States?",
+        "options": ["George Washington", "Thomas Jefferson", "Abraham Lincoln", "John Adams"],
+        "answer": "George Washington"
+    },
+    {
+        "question": "What is the largest island in the world?",
+        "options": ["Greenland", "New Guinea", "Borneo", "Madagascar"],
+        "answer": "Greenland"
+    },
+    {
+        "question": "What is the main language spoken in Egypt?",
+        "options": ["Arabic", "English", "French", "Greek"],
+        "answer": "Arabic"
+    },
+    {
+        "question": "Which country is known as the land of the midnight sun?",
+        "options": ["Norway", "Sweden", "Finland", "Iceland"],
+        "answer": "Norway"
+    },
+    {
+        "question": "What is the hardest known natural material?",
+        "options": ["Diamond", "Graphene", "Steel", "Titanium"],
+        "answer": "Diamond"
+    },
+    {
+        "question": "What is the capital of Russia?",
+        "options": ["Saint Petersburg", "Moscow", "Kazan", "Sochi"],
+        "answer": "Moscow"
+    },
+    {
+        "question": "Which planet in our solar system has the most moons?",
+        "options": ["Jupiter", "Saturn", "Mars", "Neptune"],
+        "answer": "Jupiter"
+    },
 	{
-		question: "How many legs do humans have?",
-		options: ["2", "3", "4", "5"],
-		answer: "2",
-	},
+        "question": "What is the currency of the United Kingdom?",
+        "options": ["Euro", "Pound Sterling", "Dollar", "Yen"],
+        "answer": "Pound Sterling"
+    },
+    {
+        "question": "What gas do plants absorb from the air for photosynthesis?",
+        "options": ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+        "answer": "Carbon Dioxide"
+    },
+    {
+        "question": "Who is the author of 'Pride and Prejudice'?",
+        "options": ["Charlotte Brontë", "Jane Austen", "Emily Brontë", "Louisa May Alcott"],
+        "answer": "Jane Austen"
+    },
+    {
+        "question": "In which country are the Great Pyramids of Giza located?",
+        "options": ["Mexico", "Peru", "Egypt", "India"],
+        "answer": "Egypt"
+    },
+    {
+        "question": "Which is the largest mammal in the world?",
+        "options": ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+        "answer": "Blue Whale"
+    },
+    {
+        "question": "What is the capital city of Japan?",
+        "options": ["Osaka", "Kyoto", "Tokyo", "Hiroshima"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "What is the primary ingredient in chocolate?",
+        "options": ["Milk", "Cocoa", "Sugar", "Butter"],
+        "answer": "Cocoa"
+    },
+    {
+        "question": "In which year did World War II end?",
+        "options": ["1945", "1940", "1950", "1939"],
+        "answer": "1945"
+    },
+    {
+        "question": "What is the name of the longest river in the world?",
+        "options": ["Nile", "Amazon", "Yangtze", "Mississippi"],
+        "answer": "Nile"
+    },
+    {
+        "question": "Who was the first woman to win a Nobel Prize?",
+        "options": ["Marie Curie", "Rosalind Franklin", "Ada Lovelace", "Barbara McClintock"],
+        "answer": "Marie Curie"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
+        "answer": "Mars"
+    },
+    {
+        "question": "What is the tallest building in the world?",
+        "options": ["Shanghai Tower", "Burj Khalifa", "One World Trade Center", "Taipei 101"],
+        "answer": "Burj Khalifa"
+    },
+    {
+        "question": "Who invented the telephone?",
+        "options": ["Thomas Edison", "Alexander Graham Bell", "Nikola Tesla", "Albert Einstein"],
+        "answer": "Alexander Graham Bell"
+    },
+    {
+        "question": "What is the fastest land animal in the world?",
+        "options": ["Cheetah", "Lion", "Horse", "Greyhound"],
+        "answer": "Cheetah"
+    },
+    {
+        "question": "What is the national sport of Japan?",
+        "options": ["Karate", "Baseball", "Sumo Wrestling", "Judo"],
+        "answer": "Sumo Wrestling"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "options": ["Go", "Au", "Ag", "Fe"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is known for inventing the sport of golf?",
+        "options": ["England", "United States", "Scotland", "Ireland"],
+        "answer": "Scotland"
+    },
+    {
+        "question": "What is the name of the fairy tale character who slept for 100 years?",
+        "options": ["Cinderella", "Snow White", "Rapunzel", "Sleeping Beauty"],
+        "answer": "Sleeping Beauty"
+    },
+    {
+        "question": "What is the largest bone in the human body?",
+        "options": ["Skull", "Femur", "Spine", "Humerus"],
+        "answer": "Femur"
+    },
+    {
+        "question": "In which country did the Olympic Games originate?",
+        "options": ["Greece", "Italy", "France", "United Kingdom"],
+        "answer": "Greece"
+    },
 	{
-		question: "Where is Hyderabad located??",
-		options: ["Australia", "China", "USA", "India"],
-		answer: "India",
-	},
-	{
-		question: "Which Planet do we live on?",
-		options: ["Mars", "Saturn", "Earth", "Jupiter"],
-		answer: "Earth",
-	},
-	{
-		question: "Who is the current Prime Minister of India?",
-		options: [
-			"Narendra Modi",
-			"Rahul Gandhi",
-			"Mamatha Benerjee",
-			"Aravind Kejerwal",
-		],
-		answer: "Narendra Modi",
-	},
-	{
-		question: "Which IPL has won the most number of trophies?",
-		options: ["RCB", "KKR", "MI", "CSK"],
-		answer: "MI",
-	},
-	{
-		question: "What is the first color in a rainbow?",
-		options: ["Violet", "Blue", "Yellow", "Red"],
-		answer: "Red",
-	},
-	{
-		question: "How many zeroes are there in one hundred thousand?",
-		options: ["3", "6", "5", "4"],
-		answer: "5",
-	},
-	{
-		question: "How many months have 31 days in a year?",
-		options: ["4", "5", "6", "7"],
-		answer: "7",
-	},
-	{
-		question: "Which is the most widely spoken language in the world?",
-		options: ["Hindi", "Russian", "English", "Mandarin"],
-		answer: "Mandarin",
-	},
-	{
-		question: "Which is the largest democracy in the world?",
-		options: ["South Africa", "United States", "Russia", "India"],
-		answer: "India",
-	},
-	{
-		question: "Which is the largest plateau in the world??",
-		options: [
-			"Tibatean plateau",
-			"Colorado Plateau",
-			"Altiplano",
-			"Laurentian Plateau",
-		],
-		answer: "Tibatean plateau",
-	},
-	{
-		question: " Which language is used by the computer to process data?",
-		options: ["C", "C++", "Java", "Binary"],
-		answer: "Binary",
-	},
-	{
-		question: "Which instrument is used for measuring wind speed??",
-		options: ["Galvanometer", "Manometer", "Anemometer", "Barometer"],
-		answer: "Anemometer",
-	},
-	{
-		question: "Who painted the Mona Lisa?",
-		options: ["Leonardo de Caprio", "Leonardo da Vinci", "Mona Lisa", "Picaso"],
-		answer: "Leonardo da Vinci",
-	},
-	{
-		question: "How many years are there in a millennium?",
-		options: ["1000", "100", "10000", "100000"],
-		answer: "1000",
-	},
-	{
-		question: "Which gas is most abundant in the earth’s atmosphere?",
-		options: ["Hydrogen", "Carbon dioxide", "Oxygen", "Nitrogen"],
-		answer: "Nitrogen",
-	},
-	{
-		question: "Which of following superheroes got his power from spider bite?",
-		options: ["Hulk", "Ant-man", "Venom", "Spiderman"],
-		answer: "Spiderman",
-	},
+        "question": "What is the capital of France?",
+        "options": ["Berlin", "London", "Paris", "Rome"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the main ingredient in guacamole?",
+        "options": ["Tomato", "Avocado", "Lime", "Onion"],
+        "answer": "Avocado"
+    },
+    {
+        "question": "In which year did the Titanic sink?",
+        "options": ["1912", "1920", "1905", "1915"],
+        "answer": "1912"
+    },
+    {
+        "question": "Who wrote 'Hamlet'?",
+        "options": ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+        "answer": "William Shakespeare"
+    },
+    {
+        "question": "What is the largest planet in our Solar System?",
+        "options": ["Earth", "Jupiter", "Saturn", "Mars"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "Which element has the chemical symbol 'O'?",
+        "options": ["Gold", "Oxygen", "Silver", "Iron"],
+        "answer": "Oxygen"
+    },
+    {
+        "question": "What is the hardest natural substance on Earth?",
+        "options": ["Diamond", "Gold", "Iron", "Quartz"],
+        "answer": "Diamond"
+    },
+    {
+        "question": "Who discovered penicillin?",
+        "options": ["Marie Curie", "Alexander Fleming", "Isaac Newton", "Albert Einstein"],
+        "answer": "Alexander Fleming"
+    },
+    {
+        "question": "What is the smallest country in the world?",
+        "options": ["Monaco", "Vatican City", "San Marino", "Liechtenstein"],
+        "answer": "Vatican City"
+    },
+    {
+        "question": "Which animal is known as the 'Ship of the Desert'?",
+        "options": ["Camel", "Horse", "Elephant", "Lion"],
+        "answer": "Camel"
+    },
+    {
+        "question": "In which city is the famous Colosseum located?",
+        "options": ["Paris", "Rome", "Athens", "London"],
+        "answer": "Rome"
+    },
+    {
+        "question": "What is the chemical formula for water?",
+        "options": ["CO2", "H2O", "O2", "NaCl"],
+        "answer": "H2O"
+    },
+    {
+        "question": "What does WWW stand for in a website browser?",
+        "options": ["World Wide Web", "Wild Wild West", "World Wide Wander", "Wide Web World"],
+        "answer": "World Wide Web"
+    },
+    {
+        "question": "How many players are there in a standard soccer team?",
+        "options": ["11", "10", "12", "9"],
+        "answer": "11"
+    },
+    {
+        "question": "Which country is known as the Land of the Rising Sun?",
+        "options": ["China", "South Korea", "Japan", "India"],
+        "answer": "Japan"
+    },
+    {
+        "question": "What is the main ingredient in bread?",
+        "options": ["Sugar", "Butter", "Flour", "Eggs"],
+        "answer": "Flour"
+    },
+    {
+        "question": "Who was the first person to walk on the moon?",
+        "options": ["Buzz Aldrin", "Yuri Gagarin", "Neil Armstrong", "John Glenn"],
+        "answer": "Neil Armstrong"
+    },
+    {
+        "question": "What is the currency of Japan?",
+        "options": ["Yen", "Won", "Dollar", "Euro"],
+        "answer": "Yen"
+    },
+    {
+        "question": "Which ocean is the largest?",
+        "options": ["Atlantic", "Indian", "Pacific", "Arctic"],
+        "answer": "Pacific"
+    },
+    {
+        "question": "What is the boiling point of water?",
+        "options": ["90°C", "100°C", "80°C", "120°C"],
+        "answer": "100°C"
+    }
+   
 ];
+
+
+	
 function Getquestions() {
 	var result = [];
 	var myset = new Set();
